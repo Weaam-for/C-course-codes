88d1524d391ce2c9300ca1f41b316f04a2f66499
WWS Al-humadi <W.W.S.Al-humadi@edu.salford.ac.uk>
2017-10-12 23:57:16 +0100

    First C# Programm
    This is my first C# programm

---
+++ First Program/First Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is my first program in C#
+            Console.WriteLine(" Hellow World");
+            Console.ReadKey();
+        }
+    }
+}
