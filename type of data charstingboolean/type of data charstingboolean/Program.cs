2260235b5eb8de550a012622ecdc29c0f2f1ca56
WWS Al-humadi <W.W.S.Al-humadi@edu.salford.ac.uk>
2017-10-13 20:07:27 +0100

    This is data types character string and boolean
    This is a program to explain data types in c#

---
+++ type of data charstingboolean/type of data charstingboolean/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace type_of_data_charstingboolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CharMin = Char.MinValue;
+            int CharMax = Char.MaxValue;
+
+            Char myVar = 'G';
+            // TO know what is the variable number (alskicode)
+            int myVarValue = 'G';
+            // TO Know is the number (71) it is reserve to character (G)(note you can changeConvert.ToChar(myVarValue)and put inside the brackets numbers like (71 or 75 etc
+            Char myVarValue2 = Convert.ToChar(myVarValue);
+            Console.WriteLine(" Min ={0} Max = {1}  my character = {2} my variable number = {3} my conversion value is {4}", CharMin, CharMax, myVar, myVarValue, myVarValue2);
+            // to identify variable string type
+            String myString = "this is my variable";
+            Console.WriteLine("\n my string is {0}", myString);
+            // to identify variable boolean type
+            bool myBool = false;
+            Console.WriteLine("my boolean value is{0}", myBool);
+
+            Console.ReadKey();
+        }
+    }
+}
