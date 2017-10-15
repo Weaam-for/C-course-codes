a7826024b33363ef395caff8833018f868b8e8c3
WWS Al-humadi <W.W.S.Al-humadi@edu.salford.ac.uk>
2017-10-15 02:02:57 +0100

    Switch statement
    program explain how to use switch statement to compare state of variable which must equal to one of them as well as the state of the variable should be same value of varible such as if variable interger, the variable will be integer

---
+++ Switch Statement/Switch Statement/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = "Cat";
+
+            switch (animal)
+            {
+              case  "canary":
+                   Console.WriteLine("animal is canary brid");
+                   break;
+              case  "Cat":
+                    Console.WriteLine("animal is a cat");
+                    break;
+              case "Camel":
+                    Console.WriteLine("animal is a camel");
+                    break;
+                default:
+                Console.WriteLine("non of them");
+                break;
+            }
+            int VAR = 25;
+            switch (VAR)
+            {
+               case 10:
+                    Console.WriteLine("vlue is 10");
+                    break;
+               case 7:
+                    Console.WriteLine("value is 7");
+                    break;
+               case 4:
+                    Console.WriteLine("value is 4");
+                    break;
+               default:
+                    Console.WriteLine( "no value");
+                    break;
+
+            }
+            string whathesay = "Hello";
+            switch (whathesay)
+            {
+               case "hello":
+                    Console.WriteLine(" hello");
+                    break;
+                case "Bye":
+                    Console.WriteLine("Bye");
+                    break;
+                case "welcome":
+                    break;
+                default:
+                    Console.WriteLine("no one");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
