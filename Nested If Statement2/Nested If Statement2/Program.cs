f775503dd073ad3a4d5524af92d72f3e4fc7ba04
WWS Al-humadi <W.W.S.Al-humadi@edu.salford.ac.uk>
2017-10-15 01:15:59 +0100

    Nested if statement2
    program explain nested if statement and show when the condition excuted then any statement after that will igore

---
+++ Nested If Statement2/Nested If Statement2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_If_Statement2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 4, var2 = 6;
+            if (var <= 3 && var2 >=5)
+            {
+                Console.WriteLine("condition one right");
+            }
+            else if (var <= 4 && var2 >= 6)
+            {
+                Console.WriteLine("conditions two right");
+            }
+            else if (var2 > 5)
+            {
+                Console.WriteLine("conditions three right");
+            }
+            else
+            {
+                Console.WriteLine("no one of the condition");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
