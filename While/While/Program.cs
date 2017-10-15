f69cab76102fdb571f0c780f113996cf2ba3be0d
WWS Al-humadi <W.W.S.Al-humadi@edu.salford.ac.uk>
2017-10-15 21:26:55 +0100

    this program about the second while and it use
    program decribe while and how it use in the code with exmple

---
+++ While/While/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // you must identify double as formula below
+            double salary = 1000.30, total = 0.0;
+            int i = 0;
+            while (i< 15)
+            {
+                total += salary;
+                i++;
+            }
+            Console.WriteLine("salary {0}", total);
+
+            //Console.ReadKey();
+            // Q/ find how many exercieses do need  to reach 83 Kg if  your wieght was 120kg and each exercise will make you lose 1Kg and print the result?
+            // double wieght = 120.0, 
+
+            int wieght = 120, wieghtreguired = 83;
+            int j = 0;
+            while( wieght> 83 )
+            {
+                wieght--;
+                j++;
+            }
+            Console.WriteLine(" excercise no. is {0}", j);
+            Console.ReadKey();
+
+               
+                
+        }
+    }
+}
