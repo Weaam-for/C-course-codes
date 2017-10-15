d3920e4d18c73278d1c6cb14025a1db0172b3c62
WWS Al-humadi <W.W.S.Al-humadi@edu.salford.ac.uk>
2017-10-15 21:55:45 +0100

    This program explain how to use for loop
    this is very simple program explain how to use for loop

---
+++ Forloop/Forloop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int salary = 1000, total = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                total += salary;
+                Console.WriteLine("salary for 12 months {0}", total);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
