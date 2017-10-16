2c699aa3eddd6ee0bb5be8002fbe2e574c47e43f
WWS Al-humadi <W.W.S.Al-humadi@edu.salford.ac.uk>
2017-10-16 21:04:15 +0100

    this program explain breack loop -Continue command

---
+++ Break loop- Continue command/Break loop- Continue command/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_loop__Continue_command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++ )
+            {
+                Console.WriteLine("hi");
+                if (i > 2)
+                    continue;
+                for ( int j = 0; j<=4; j++)
+                {
+                    Console.WriteLine(" hello");
+                   
+                }
+               // Console.WriteLine("hi");
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
